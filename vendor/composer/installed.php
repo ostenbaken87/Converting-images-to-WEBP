4ef54854d1da61c2593572780191a8f34017c27a
ostenbaken87 <ostenbaken87@mail.ru>
2024-05-22 11:40:59 +0800

    update composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'tng-n/convert-to-webp',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bd4898e23341a930af4051546c83333fba4a5955',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'tng-n/convert-to-webp' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bd4898e23341a930af4051546c83333fba4a5955',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
